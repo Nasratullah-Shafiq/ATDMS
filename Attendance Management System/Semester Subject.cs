a2b641cdf813c11a8a8275a4569ad01bb93b9449
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-11-12 19:48:05 +0430

    new files added

---
+++ Attendance Management System/Semester Subject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Attendance_Management_System
+{
+    public partial class Semester_Subject : Form
+    {
+        public Semester_Subject()
+        {
+            InitializeComponent();
+        }
+    }
+}
